2c7f8e2ce2a29084170def4483e96d2b94d91ab1
Freddd13 <50367552+Freddd13@users.noreply.github.com>
2024-06-21 22:16:55 +0800

    fix: add error ignore for email attachment encoding

--- bili_backup/email.py
+++ bili_backup/email.py
@@ -1,7 +1,7 @@
 '''
 Date: 2023-10-23 18:24:50
 LastEditors: Kumo
-LastEditTime: 2024-06-20 12:58:58
+LastEditTime: 2024-06-21 22:16:19
 Description: 
 '''
 
@@ -36,18 +36,18 @@ class EmailHandler:
         message.attach(MIMEText(content, 'plain', 'utf-8'))
         
         # sheet files
-        for file in sheet_files:
-            filename = os.path.basename(file)
-            logger.debug("sending filename: "+filename)
-            att = MIMEText(open(file, 'rb').read(), 'base64', 'utf-8')
-            att["Content-Type"] = 'application/octet-stream'
-            att["Content-Disposition"] = 'attachment; filename=' + '\"'+ filename +'\"'
-            message.attach(att)
+        # for file in sheet_files:
+        #     filename = os.path.basename(file)
+        #     logger.debug("sending filename: "+filename)
+        #     att = MIMEText(open(file, 'rb').read(), 'base64', 'utf-8')
+        #     att["Content-Type"] = 'application/octet-stream'
+        #     att["Content-Disposition"] = 'attachment; filename=' + '\"'+ filename +'\"'
+        #     message.attach(att)
         
         # log files
         for file in log_files:
             filename = os.path.basename(file)
-            att = MIMEText(open(file, 'r').read(), 'base64', 'utf-8')
+            att = MIMEText(open(file, 'r', errors='ignore').read(), 'base64', 'utf-8')
             att["Content-Type"] = 'application/octet-stream'
             att["Content-Disposition"] = 'attachment; filename=' + '\"'+ filename +'\"'
             message.attach(att)
